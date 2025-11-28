202abd9bd48c6caf168f182f1578174c2272f9e8
Mr.Robot <99020575+sanket1602@users.noreply.github.com>
2025-11-28 18:25:59 +0530

    File & Folder name changes

--- log-archive.sh
+++ log-archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Archiving $1 directory"
-mkdir -p ./archives
+mkdir -p ./archived-logs
 tar -czvf "$(pwd)/archives/logs_archive_$(date +"%Y%m%d_%H%M%S").tar.gz" -C $1 .
-echo "Saved as logs_archive_$(date +"%Y%m%d_%H%M%S").tar.gz"
+echo "File Saved as logs_archive_$(date +"%Y%m%d_%H%M%S").tar.gz"
