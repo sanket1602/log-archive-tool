f959ab5709032107cde43412fc0228d7135808e1
Mr.Robot <99020575+sanket1602@users.noreply.github.com>
2025-12-09 15:26:48 +0530

    updating echo command

--- log-archive.sh
+++ log-archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Archiving $1 directory"
+echo "Archiving $1 Directory"
 mkdir -p ./archived-logs
 tar -czvf "$(pwd)/archives/logs_archive_$(date +"%Y%m%d_%H%M%S").tar.gz" -C $1 .
 echo "File Saved as logs_archive_$(date +"%Y%m%d_%H%M%S").tar.gz"
